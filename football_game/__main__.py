ee3150b6a143760cb9e411f639e530ed63bdc80e
railnlays <juliomdp92@gmail.com>
2024-02-09 17:59:29 +0100

    publish. CSV to DB fixing problem

--- football_game/__main__.py
+++ football_game/__main__.py
@@ -12,13 +12,48 @@ def main():
     SQLModel.metadata.create_all(engine)
 
     with Session(engine) as session:
-        with open("test_player.csv", newline="") as csvfile:
+        
+        csv_name = "football_game/schemas/test_player.csv"
+
+        PlayerCSVToDB().create_csv(
+            csv_name, 
+            "name",
+            "age",
+            "weight",
+            "height",
+            "salary",
+            "position",
+            "pac",
+            "sho",
+            "pas",
+            "dri",
+            "defe",
+            "phy",
+            "goalkeeping",
+        )
+
+        with open(csv_name, newline="") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
+                row["name"] = str(row["name"])
+                row["age"] = int(row["age"])
+                row["weight"] = int(row["weight"])
+                row["height"] = float(row["height"])
+                row["salary"] = float(row["salary"])
+                row["position"] = str(row["position"])
+                row["pac"] = float(row["pac"])
+                row["sho"] = float(row["sho"])
+                row["pas"] = float(row["pas"])
+                row["dri"] = float(row["dri"])
+                row["defe"] = float(row["defe"])
+                row["phy"] = float(row["phy"])
+                row["goalkeeping"] = float(row["goalkeeping"])
+
                 player = Player(**row)
                 session.add(player)
-            session.commit()
-            print("Data inserted successfully!")
+
+        session.commit()
+        print("Data inserted successfully!")
 
 
 if __name__ == "__main__":
--- football_game/schemas/__init__.py
+++ football_game/schemas/__init__.py
@@ -1,12 +1,14 @@
 from ._player import Player
 from ._team import Team
 from ._trainer import Trainer
+from ._player_DB_update import PlayerCSVToDB
 
 
 __all__ = [
     "Player",
     "Team",
     "Trainer",
+    "PlayerCSVToDB",
     # "CenterBack",
     # "CenterRightBack",
     # "CenterLeftBack",
